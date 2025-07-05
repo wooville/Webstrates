187655df4730d394771c94f5a4a011e7b5fd1a09
Julian Woo <julian.wooville@gmail.com>
2025-07-05 19:36:19 -0400

    phew

--- tests/config.js
+++ tests/config.js
@@ -5,7 +5,7 @@ module.exports = {
 
 	// Auth credentials
 	authType: 'au', // One of 'github', 'au', ...
-	server_address: 'http://localhost:443/',
+	server_address: 'http://localhost:80/',
 	username: '',
 	password: ''
 };
